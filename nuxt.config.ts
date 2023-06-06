04d4719024aacd1ce2c500beebe4f8008f704fc5
EC2 Default User <ec2-user@ip-172-31-83-58.ec2.internal>
2023-06-06 15:03:09 +0000

    add redis config and test api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,22 @@ export default defineNuxtConfig({
 			autoprefixer: {},
 		},
 	},
+	nitro: {
+		storage: {
+		redis: {
+			driver: 'redis',
+			port: 6379, // Redis port
+			host: "127.0.0.1", // Redis host
+			username: "", // needs Redis >= 6
+			password: "",
+			db: 0, // Defaults to 0
+			tls: {}
+			/* redis connector options */
+		},
+		// db: {
+		// 	driver: 'fs',
+		// 	base: './.data/db'
+		// }
+		}
+	}	  
 })
